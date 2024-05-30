fa6f92fbc170e17d9d62799255c90693ff94eccd
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-30 13:21:16 +0700

    kondisi jika front > rear

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -80,6 +80,15 @@ public:
             }
             cout << endl;
         }
+        else {
+            //jika FROMT > REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1) {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+        }
+
+        FRONT_Position = 0;
 
 
 
