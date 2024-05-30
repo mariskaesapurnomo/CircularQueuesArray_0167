899c281bfe85ad8beb91f21c9a12946bb5484878
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-30 10:45:33 +0700

    variabel class queues

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -4,8 +4,13 @@
 #include <iostream>
 using namespace std;
 
+class Queues {
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
