521d92f4a2c230bf1278c10e6542445831f1e9fc
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-05-30 13:10:14 +0700

    kondisi lainnya jika REAR berada di posisi trakhir array

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -29,6 +29,18 @@ public:
 			FRONT = 0;
 			REAR = 0;
 		}
+        else {
+            //jika REAR berapa di posisi terakhir array, kembali ke awla array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+
+    }
+
+	
 	
 
 int main()
